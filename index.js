20dbab945b6443f61607090dfd7eb2c6d53b59ed
Anthony Ngonde <anto.ngonde@gmail.com>
2024-04-03 15:54:04 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+    //let names = ["Charlie", "Samip", "Ali"];
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages;
+}
+
+
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+
+    while (currentNumber >= 0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
